5a611c20840bf2ebaf6728129e4dfda8f1162a1f
bendn <bend.n@outlook.com>
2024-12-07 21:55:54 +0700

    6

--- src/lib.rs
+++ src/lib.rs
@@ -25,113 +25,187 @@
     portable_simd
 )]
 mod util;
-pub mod day5 {
+pub mod day6 {
     use crate::util::prelude::*;
-    struct Setz {
-        bits: [u128; 100],
+
+    const SIZE: usize = 130;
+
+    #[derive(Clone, Copy)]
+    struct bitset17030([u64; 267]);
+    impl bitset17030 {
+        const fn new() -> Self {
+            Self([0; 267])
+        }
+        #[inline(always)]
+        fn set(&mut self, index: usize) {
+            unsafe { *self.0.get_unchecked_mut(index >> 6) |= 1u64.wrapping_shl(index as u32) };
+        }
+        #[inline(always)]
+        fn get(&self, index: usize) -> bool {
+            unsafe { *self.0.get_unchecked(index >> 6) & 1u64.wrapping_shl(index as u32) != 0 }
+        }
+        #[inline(always)]
+        fn popcnt(self) -> u32 {
+            self.0.into_iter().map(u64::count_ones).sum::<u32>()
+        }
     }
-    impl Setz {
-        fn new() -> Self {
-            Self { bits: [0; 100] }
+
+    // type bits = bitvec::BitArr!(for 16960, in u64);
+    // #[derive(Clone, Copy)]
+    // struct bitset17030(bits);
+    // impl bitset17030 {
+    //     fn new() -> Self {
+    //         Self(bits::default())
+    //     }
+    //     #[inline(always)]
+    //     #[no_mangle]
+    //     fn set(&mut self, index: usize) {
+    //         unsafe { self.0.set_unchecked(index, true) };
+    //     }
+    //     #[inline(always)]
+    //     #[no_mangle]
+    //     fn get(&self, index: usize) -> bool {
+    //         unsafe { *self.0.get_unchecked(index) }
+    //     }
+    //     #[inline(always)]
+    //     fn popcnt(self) -> u32 {
+    //         self.0.count_ones() as _
+    //     }
+    // }
+
+    fn follow(i: &[u8]) -> (i64, bitset17030) {
+        let mut marked = bitset17030::new();
+        let mut position = memchr::memchr(b'^', i).ψ() as i64;
+        let guard = position;
+        let mut pointing = 0u8;
+
+        'outer: loop {
+            match pointing {
+                0 => loop {
+                    marked.set(position as usize);
+                    let check = position - SIZE as i64 - 1;
+                    if check < 0 {
+                        break 'outer;
+                    }
+                    match C! { i[check as usize] } {
+                        b'\n' => break 'outer,
+                        b'#' => break,
+                        _ => position = check,
+                    };
+                },
+                1 => loop {
+                    marked.set(position as usize);
+                    let check = position + 1;
+                    match C! { i[check as usize] } {
+                        b'\n' => break 'outer,
+                        b'#' => break,
+                        _ => position = check,
+                    };
+                },
+                2 => loop {
+                    marked.set(position as usize);
+                    let check = position + SIZE as i64 + 1;
+                    match C! { i[check as usize] } {
+                        b'\n' => break 'outer,
+                        b'#' => break,
+                        _ => position = check,
+                    };
+                },
+                3 => loop {
+                    marked.set(position as usize);
+                    let check = position - 1;
+                    if check < 0 {
+                        break 'outer;
+                    }
+                    match C! { i[check as usize] } {
+                        b'\n' => break 'outer,
+                        b'#' => break,
+                        _ => position = check,
+                    };
+                },
+                _ => shucks!(),
+            }
+            pointing += 1;
+            pointing %= 4;
         }
-        fn insert(&mut self, a: u8, b: u8) {
-            unsafe { *self.bits.get_unchecked_mut(a as usize) |= 1 << b };
-        }
-        fn test(&self, a: u8, b: u8) -> bool {
-            unsafe { (*self.bits.get_unchecked(a as usize) & 1 << b) != 0 }
-        }
+        (guard, marked)
     }
 
     #[no_mangle]
     pub fn part1(i: &str) -> impl Display {
-        let i = i.as_bytes();
-        let c = unsafe { C! { &i[..1176 * 6]}.as_chunks_unchecked::<6>() };
-        let mut bitsets = Setz::new();
-        for i in 0..1176 {
-            let [a, b, _, c, d, _] = C! { c[i] };
-            bitsets.insert((a - b'0') * 10 + (b - b'0'), (c - b'0') * 10 + (d - b'0'));
-        }
-        let mut sum = 0;
-        let mut v = Vec::with_capacity(25);
-        'out: for mut pages in C!(&i[1176 * 6 + 1..]).行() {
-            v.clear();
-            let [a, b, rest @ ..] = pages else {
-                unsafe { std::hint::unreachable_unchecked() }
-            };
-            pages = rest;
-            v.push((a - b'0') * 10 + (b - b'0'));
-            let mut i = 0;
-            loop {
-                mat!(pages {
-                    [b',', a, b, rest @ ..] => {
-                        v.push((a - b'0') * 10 + (b - b'0'));
-                        if let &[a, b] = C!{ &v[i..] } {
-                            // valid ones always have a rule
-                            if !bitsets.test(a, b) {
-                                continue 'out;
-                            }
-                            i += 1;
-                        }
-                        pages = rest;
-                    },
-                    [] => break,
-                })
-            }
-            sum += C! { v[(v.len() - 1) / 2] } as u32;
-        }
-        leek!(v);
-        sum
+        follow(i.as_bytes()).1.popcnt()
     }
 
     #[no_mangle]
     pub fn part2(i: &str) -> impl Display {
         let i = i.as_bytes();
-        let c = unsafe { C! { &i[..1176 * 6]}.as_chunks_unchecked::<6>() };
-        let mut bitsets = Setz::new();
-        for i in 0..1176 {
-            let [a, b, _, c, d, _] = C! { c[i]};
-            bitsets.insert((a - b'0') * 10 + (b - b'0'), (c - b'0') * 10 + (d - b'0'));
-        }
-        let mut sum = 0;
-        let mut v = Vec::with_capacity(25);
-        for mut pages in C!(&i[1176 * 6 + 1..]).行() {
-            v.clear();
-            let [a, b, rest @ ..] = pages else {
-                unsafe { std::hint::unreachable_unchecked() }
-            };
-            pages = rest;
-            v.push((a - b'0') * 10 + (b - b'0'));
-            let mut i = 0;
-            let mut faults = 0;
-            loop {
-                mat!(pages {
-                    [b',', a, b, rest @ ..] => {
-                        v.push((a - b'0') * 10 + (b - b'0'));
-                        if let &[a, b] = C! { &v[i..] } {
-                            if !bitsets.test(a, b) {
-                                faults += 1;
-                            }
-                            i += 1;
+        fn run(i: &[u8], guard: i64, obstacle: i64) -> bool {
+            let mut marked = [bitset17030::new(); 4];
+            let mut position = guard;
+            let mut pointing = 0u8;
+
+            macro_rules! check {
+                () => {
+                    if marked[pointing as usize].get(position as usize) {
+                        return true;
+                    } else {
+                        marked[pointing as usize].set(position as usize)
+                    }
+                };
+            }
+            macro_rules! entry {
+                (sub $check:expr) => {
+                    loop {
+                        check!();
+                        let check = $check;
+                        if check < 0 {
+                            return false;
                         }
-                        pages = rest;
-                    },
-                    [] => break,
-                })
+                        if check == obstacle {
+                            break;
+                        }
+                        match C! { i[check as usize] } {
+                            b'\n' => return false,
+                            b'#' => break,
+                            _ => position = check,
+                        };
+                    }
+                };
+                (add $check:expr) => {
+                    loop {
+                        check!();
+                        let check = $check;
+                        if check == obstacle {
+                            break;
+                        }
+                        match i.get(check as usize) {
+                            None | Some(b'\n') => return false,
+                            Some(b'#') => break,
+                            _ => position = check,
+                        };
+                    }
+                };
             }
-            if faults == 0 {
-                continue;
-            }
-            let mid = (v.len() - 1) / 2;
-            let (_, &mut mid, _) = v.select_nth_unstable_by(mid, |&a, &b| {
-                if bitsets.test(a, b) {
-                    std::cmp::Ordering::Greater
-                } else {
-                    std::cmp::Ordering::Less
+
+            loop {
+                match pointing {
+                    0 => entry!(sub position - SIZE as i64 - 1),
+                    1 => entry!(add position + 1),
+                    2 => entry!(add position + SIZE as i64 + 1),
+                    3 => entry!(sub position - 1),
+                    _ => shucks!(),
                 }
-            });
-            sum += mid as u32;
+                pointing += 1;
+                pointing %= 4;
+            }
         }
-        leek!(v);
-        sum
+        let (guard, marked) = follow(&i);
+        use rayon::prelude::*;
+        (0..SIZE * (SIZE + 1))
+            .filter(|&i| marked.get(i))
+            .filter(|&j| C! { i[j] } == b'.')
+            .par_bridge()
+            .filter(|&j| run(&i, guard, j as i64))
+            .count()
     }
 }
